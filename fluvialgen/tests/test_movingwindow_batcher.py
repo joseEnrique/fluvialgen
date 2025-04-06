ef81e5ac8dd95e0adef2ed6e5b4e208a61e185e8
Jose Enrique <joseenriqueruiznavarro@gmail.com>
2025-04-06 21:13:21 +0200

    changed tests

--- fluvialgen/tests/test_movingwindow_batcher.py
+++ fluvialgen/tests/test_movingwindow_batcher.py
@@ -30,7 +30,7 @@ class TestMovingWindowBatcher(unittest.TestCase):
         X, y = batcher.get_message()
 
         # Verify that X has the correct shape (batch_size rows)
-        self.assertEqual(X.shape[0], self.batch_size)
+        self.assertEqual(X.shape[0], self.batch_size * self.instance_size)
         
         # Verify that y has the correct length (batch_size * instance_size)
         self.assertEqual(len(y), self.batch_size * self.instance_size)
