328bed5ba4e5be33a49789b74b4b539b1a39bd07
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2025-05-10 16:54:19 +0200

    added MockData

--- fluvialgen/tests/test_movingwindow_batcher.py
+++ fluvialgen/tests/test_movingwindow_batcher.py
@@ -1,19 +1,61 @@
 import unittest
 import numpy as np
 import pandas as pd
-from river import datasets
+from datetime import datetime
 
-#sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from fluvialgen.movingwindow_generator import MovingWindowBatcher
+
+class MockDataset:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        item = self.data[self.index]
+        self.index += 1
+        return item
+
+    def take(self, n):
+        """Return an iterator that yields n items from the dataset."""
+        class TakeIterator:
+            def __init__(self, data, n):
+                self.data = data
+                self.n = n
+                self.index = 0
+
+            def __iter__(self):
+                return self
+
+            def __next__(self):
+                if self.index >= self.n or self.index >= len(self.data):
+                    raise StopIteration
+                item = self.data[self.index]
+                self.index += 1
+                return item
+
+        return TakeIterator(self.data, n)
 
 class TestMovingWindowBatcher(unittest.TestCase):
     def setUp(self):
         """
         Initial setup for each test
         """
-        self.dataset = datasets.Bikes()
+        self.data = [
+            ({'moment': datetime(2024, 1, 1, 0, 0), 'value': 1}, 1),
+            ({'moment': datetime(2024, 1, 1, 0, 1), 'value': 2}, 2),
+            ({'moment': datetime(2024, 1, 1, 0, 2), 'value': 3}, 3),
+            ({'moment': datetime(2024, 1, 1, 0, 3), 'value': 4}, 4),
+            ({'moment': datetime(2024, 1, 1, 0, 4), 'value': 5}, 5),
+            ({'moment': datetime(2024, 1, 1, 0, 5), 'value': 6}, 6)
+        ]
+        self.dataset = MockDataset(self.data)
         self.instance_size = 3
-        self.batch_size = 3
+        self.batch_size = 2
 
     def test_window_creation(self):
         """
@@ -29,12 +71,27 @@ class TestMovingWindowBatcher(unittest.TestCase):
         # Get the first batch
         X, y = batcher.get_message()
 
-        # Verify that X has the correct shape (batch_size rows)
+        # Verify that X has the correct shape (batch_size * instance_size rows)
         self.assertEqual(X.shape[0], self.batch_size * self.instance_size)
         
         # Verify that y has the correct length (batch_size * instance_size)
         self.assertEqual(len(y), self.batch_size * self.instance_size)
 
+        # Verify the content of the first window
+        self.assertEqual(X.iloc[0]['value'], 1)
+        self.assertEqual(X.iloc[1]['value'], 2)
+        self.assertEqual(X.iloc[2]['value'], 3)
+        self.assertEqual(y.iloc[0], 1)
+        self.assertEqual(y.iloc[1], 2)
+        self.assertEqual(y.iloc[2], 3)
+
+        # Verify the content of the second window
+        self.assertEqual(X.iloc[3]['value'], 2)
+        self.assertEqual(X.iloc[4]['value'], 3)
+        self.assertEqual(X.iloc[5]['value'], 4)
+        self.assertEqual(y.iloc[3], 2)
+        self.assertEqual(y.iloc[4], 3)
+        self.assertEqual(y.iloc[5], 4)
 
 if __name__ == '__main__':
     unittest.main() 
