edfa6168e1b4e96f051a4a8967efc8b7c917801d
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2025-10-03 11:10:38 +0200

    added CSV support

--- fluvialgen/__init__.py
+++ fluvialgen/__init__.py
@@ -1,3 +1,16 @@
 """
 Generator module for River datasets.
 """ 
+from .base_generator import BaseGenerator
+from .river_dataset_generator import RiverDatasetGenerator
+from .movingwindow_generator import MovingWindowBatcher
+from .past_forecast_batcher import PastForecastBatcher
+from .csv_dataset_generator import CSVDatasetGenerator
+
+__all__ = [
+    "BaseGenerator",
+    "RiverDatasetGenerator",
+    "MovingWindowBatcher",
+    "PastForecastBatcher",
+    "CSVDatasetGenerator",
+]
