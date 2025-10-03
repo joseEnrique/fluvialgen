562d79d90a0b5394adb5417525e00de593cdd6f9
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2025-10-03 12:32:16 +0200

    added CSVPastForecastBatcher

--- fluvialgen/__init__.py
+++ fluvialgen/__init__.py
@@ -6,6 +6,7 @@ from .river_dataset_generator import RiverDatasetGenerator
 from .movingwindow_generator import MovingWindowBatcher
 from .past_forecast_batcher import PastForecastBatcher
 from .csv_dataset_generator import CSVDatasetGenerator
+from .csv_past_forecast_batcher import CSVPastForecastBatcher
 
 __all__ = [
     "BaseGenerator",
@@ -13,4 +14,5 @@ __all__ = [
     "MovingWindowBatcher",
     "PastForecastBatcher",
     "CSVDatasetGenerator",
+    "CSVPastForecastBatcher",
 ]
